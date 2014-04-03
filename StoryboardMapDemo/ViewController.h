88028e991aabc43fb3dfb53a1c31699fcfbee24f
Ricardo Rubio <rickarubio@gmail.com>
2014-04-02 19:31:03 -0700

    adds UIElements as properties to the ViewController

--- StoryboardMapDemo/ViewController.h
+++ StoryboardMapDemo/ViewController.h
@@ -9,5 +9,7 @@
 #import <UIKit/UIKit.h>
 
 @interface ViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UILabel *addressLabel;
+@property (strong, nonatomic) IBOutlet UIImageView *photoImageView;
 
 @end
