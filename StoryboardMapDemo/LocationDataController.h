396e2f256e7ef7305fd08fa7261490036b8e49ea
Ricardo Rubio <rickarubio@gmail.com>
2014-04-02 19:15:31 -0700

    creates controller class for location

---
+++ StoryboardMapDemo/LocationDataController.h
@@ -0,0 +1,13 @@
+//
+//  LocationDataController.h
+//  StoryboardMapDemo
+//
+//  Created by Ricardo Rubio on 4/2/14.
+//  Copyright (c) 2014 Ricardo Rubio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface LocationDataController : NSObject
+
+@end
---
+++ StoryboardMapDemo/LocationDataController.m
@@ -0,0 +1,13 @@
+//
+//  LocationDataController.m
+//  StoryboardMapDemo
+//
+//  Created by Ricardo Rubio on 4/2/14.
+//  Copyright (c) 2014 Ricardo Rubio. All rights reserved.
+//
+
+#import "LocationDataController.h"
+
+@implementation LocationDataController
+
+@end
