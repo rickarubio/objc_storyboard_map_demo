89ab24cfd7ae9e5fa1db93e92d6752a2d4d57011
Ricardo Rubio <rickarubio@gmail.com>
2014-04-02 19:08:31 -0700

    Adds a view for a map, adds a button to home view to navigate to map view

---
+++ StoryboardMapDemo/MapViewController.h
@@ -0,0 +1,13 @@
+//
+//  MapViewController.h
+//  StoryboardMapDemo
+//
+//  Created by Ricardo Rubio on 4/2/14.
+//  Copyright (c) 2014 Ricardo Rubio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MapViewController : UIViewController
+
+@end
