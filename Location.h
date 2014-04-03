22cf20ae9a9c4e01252f23d88e52e080a2b3b4e1
Ricardo Rubio <rickarubio@gmail.com>
2014-04-02 19:12:46 -0700

    creates a class to represent a physical location

---
+++ Location.h
@@ -0,0 +1,13 @@
+//
+//  Location.h
+//  StoryboardMapDemo
+//
+//  Created by Ricardo Rubio on 4/2/14.
+//  Copyright (c) 2014 Ricardo Rubio. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Location : NSObject
+
+@end
---
+++ Location.m
@@ -0,0 +1,13 @@
+//
+//  Location.m
+//  StoryboardMapDemo
+//
+//  Created by Ricardo Rubio on 4/2/14.
+//  Copyright (c) 2014 Ricardo Rubio. All rights reserved.
+//
+
+#import "Location.h"
+
+@implementation Location
+
+@end
